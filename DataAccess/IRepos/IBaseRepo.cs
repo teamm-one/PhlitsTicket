ad6fc84c8b6ec06d9ee401b7d7ca35a9aeeabd7b
Adel Fayed <fayedadel627@gmail.com>
2024-11-29 20:16:36 +0200

    Create BaseRepo

---
+++ DataAccess/IRepos/IBaseRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.IRepos
+{
+    internal interface IBaseRepo <T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        void Create(T entity);
+        void Edit(T entity);
+        void Delete(int id);
+    }
+}
---
+++ DataAccess/Repos/BaseRepo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.IRepos;
+using DataAccess.ApplicationDbContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Repos
+{
+    internal class BaseRepo<T> : IBaseRepo<T> where T : class
+    {
+        private readonly DbContext _context;
+        public BaseRepo(DbContext context)
+        {
+            _context = context;
+        }
+
+        IEnumerable<T> IBaseRepo<T>.GetAll()
+        {
+           return _context.Set<T>().ToList();
+        }
+
+        T IBaseRepo<T>.GetById(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        void IBaseRepo<T>.Create(T entity)
+        { 
+               _context.Set<T>().Add(entity);
+              _context.SaveChanges();
+        }
+
+    
+
+        void IBaseRepo<T>.Edit(T entity)
+        {
+            _context.Set<T>().Update(entity);
+        }
+
+        void IBaseRepo<T>.Delete(int id)
+        {
+            var entity = _context.Set<T>().Find(id);
+            _context.Set<T>().Remove(entity);
+            _context.SaveChanges();
+        }
+
+       
+    }
+}
